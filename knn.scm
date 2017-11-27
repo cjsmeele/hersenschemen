fdb6b5e1905e0c212147073dff752e671bb2da6a
Chris Smeele <cj.smeele@xs4all.nl>
2017-11-27 14:20:30 +0100

    Reimplemented KNN in Scheme, for performance reasons
    Unfortunately the Perl6 implementation would have taken hours to grade
    different K values. This version is like orders of magnitude faster.
    
    And hey, at least now our repo name makes sense again.

---
+++ knn.scm
@@ -0,0 +1,109 @@
+#!/usr/bin/env gosh
+
+;; CSV filename => 2d list by row,col.
+;; Cells are converted to numbers.
+(define (parse-csv-file filename)
+  (let loop ((p (open-input-file filename))
+             (data '()))
+    (let ((v (read-line p)))
+      (if (eof-object? v)
+          (reverse data)
+          (loop p (cons (map string->number (string-split v #\;))
+                        data))))))
+
+;; Calculate the distance between two points in n-dimensional space.
+(define (euclidean-distance p1 p2)
+  (sqrt (reduce + 0 (map (lambda (x) (expt x 2))
+                         (map - p1 p2)))))
+
+;; From a list, return the value that occurs the most often.
+;; Keep popping the list until exactly one value has the most occurrences.
+(define (pick-favorite ordered-values)
+  (letrec* ((uniques (delete-duplicates ordered-values))
+            (uniques-counts
+             (map (lambda (uv) (count (lambda (ov) (equal? ov uv)) ordered-values))
+                  uniques))
+            (count-max (apply max uniques-counts)))
+    (if (= 1 (count (lambda (c) (= c count-max)) uniques-counts))
+        (any (lambda (v c) (and (= count-max c) v))
+             uniques
+             uniques-counts)
+        (pick-favorite (take ordered-values
+                             (- (length ordered-values) 1))))))
+
+;; Map a YYYYMMDD date number (e.g. 20170131) to a season symbol.
+(define (date-to-season date-nr)
+  ;; Extract the month and match it.
+  (ecase (div (modulo date-nr 10000)
+             100)
+    ((12  1  2) 'winter)
+    (( 3  4  5) 'spring)
+    (( 6  7  8) 'summer)
+    (( 9 10 11) 'autumn)))
+
+;; Given base data with known dates, and input data with unknown dates,
+;; determine the season for each input datum using the KNN algorithm.
+(define (classify-it k base-data input-data)
+  (let ((base-seasons (map (lambda (entry)
+                             (date-to-season (car entry)))
+                           base-data)))
+    (map (lambda (input)
+           (pick-favorite
+            (take ;; Take K seasons from list ordered by distance.
+             (map cadr
+                  (sort
+                   ;; => ((distance season)...)
+                   (map (lambda (base its-season)
+                          (list (euclidean-distance (cdr base) (cdr input))
+                                its-season))
+                        base-data
+                        base-seasons)
+                   (lambda (a b)
+                     ;; Sort by distance, near to far.
+                     (<= (car a) (car b)))))
+             k)))
+         input-data)))
+
+;; Run classify-it with data read from CSV files and print the results.
+(define (classify k base-csv input-csv)
+  (let ((input-seasons
+         (classify-it k
+                      (parse-csv-file base-csv)
+                      (parse-csv-file input-csv))))
+    (for-each
+     (lambda (l)
+       (display l)
+       (newline))
+     input-seasons)))
+
+;; Run classify with validation data as the input, of which the dates are known.
+;; Return a correctness ratio.
+(define (verify-it k base-data validation-data)
+  ;; TODO.
+  (display "UNIMPLEMENTED\n")
+  0.0)
+
+(define (verify k base-csv validation-csv)
+  (format #t "~a%~%" (floor (* 100
+                               (verify-it k
+                                          (parse-csv-file base-csv)
+                                          (parse-csv-file validation-csv))))))
+
+(define (usage program-name)
+  (format (current-error-port)
+          "usage: ~a <classify | verify> k base-csv <input-csv | validation-csv>\n"
+          program-name)
+  (exit 2))
+
+(define (main args)
+  (when (< (length args) 2)
+    (usage (car args)))
+  (cond ((and (string=? "classify" (cadr args))
+              (= 5 (length args)))
+         (apply classify (cons (string->number (caddr args))
+                               (cdddr args))))
+        ((and (string=? "verify" (cadr args))
+              (= 5 (length args)))
+         (apply verify (cons (string->number (caddr args))
+                             (cdddr args))))
+        (else (usage (car args)))))
