e2eea2b4e072673d166a29d49bcd03bcd9409ee4
ManDeJan <janhlsm@gmail.com>
2017-12-15 14:30:29 +0100

    Implemented Naïve algebraic neural net

--- neuralnet-f/main.cc
+++ neuralnet-f/main.cc
@@ -7,28 +7,47 @@
 
 using namespace nn;
 
+constexpr auto g = [](auto z)  { return 1 / (1 + exp(-z)); };
+constexpr auto g_ = [](auto z) { return g(z) * (1 - g(z)); };
+
 int main() {
     srand(time(NULL));
     std::cout.precision(2);
 
-    Matrixd<2,3> A { 1, 2, 3,
-                     4, 5, 6 };
-    Matrixd<3,2> B { 0, 5,
-                     1, 2,
-                     3, 0 };
+    Matrixd<4,3> X {
+            0, 0, 1,
+            0, 1, 1,
+            1, 0, 1,
+            1, 1, 1
+    };
 
-    auto C = B.map([](auto x) { return 1 / (1 + exp(-x)); });
-    B.mip([](auto x) { return 1 / (1 + exp(-x)); });
+    Matrixd<4,1> y {
+            0,
+            1,
+            1,
+            0
+    };
 
-    std::cout << (B * A) << "\n";
-    std::cout << (C * A) << "\n";
+    auto rd = [](auto) {return (double)rand()/RAND_MAX*2 - 1;};
+    Matrixd<3,4> w1 {};
+    w1.mip(rd);
+    Matrixd<4,1> w2 {};
+    w2.mip(rd);
 
-    // [[  2.0,  9.0 ],
-    //  [  5.0, 30.0 ]]
+    constexpr auto eta = 0.1;
 
-    // [[ 20.0, 25.0, 30.0 ],
-    //  [  9.0, 12.0, 15.0 ],
-    //  [  3.0,  6.0,  9.0 ]]
+    for (int _ = 0; _ < 60000; ++_) {
+        auto L1 = dot(X,  w1).map(g); // 4 * 4
+        auto L2 = dot(L1, w2).map(g); // 4 * 1
+        auto L2D = (y - L2) * L2.map(g_);
+        auto L1D = dot(L2D, w2.T()) * L1.map(g_);
+        w2 += eta * dot(L1.T(), L2D);
+        w1 += eta * dot( X.T(), L1D);
+    }
+
+    auto L1 = dot(X,  w1).map(g);
+    auto L2 = dot(L1, w2).map(g);
+    std::cout << L2;
 
     #ifdef NDEBUG
     std::cout << "NDEBUG\n";
