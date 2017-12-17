f3fb5bf62d24cfea4ccec37113a915c7134a9ed1
Chris Smeele <cj.smeele@xs4all.nl>
2017-12-17 01:36:41 +0100

    Tied together `net` with existing forward/backprop code

--- neuralnet-f/main.cc
+++ neuralnet-f/main.cc
@@ -25,61 +25,27 @@ int main() {
             0
     };
 
-    auto aoeu = nn::make_net<double,3,1,0,4>{};
-    char *name = abi::__cxa_demangle(typeid(aoeu).name(),
+    auto net = nn::make_net<double,3,1,1,4>{};
+    char *name = abi::__cxa_demangle(typeid(net).name(),
                                      nullptr,
                                      nullptr,
                                      nullptr);
     std::cout << "🦄🦄🦄🦄🦄🦄🦄: " << name << "\n";
 
+
     auto rd = [](auto) {return (double)rand()/RAND_MAX*2 - 1;};
-    Matrixd<3,4> w0 {};
-    w0.mip(rd);
-    Matrixd<4,1> w1 {};
-    w1.mip(rd);
+    std::apply([&rd](auto& ...x){(x.mip(rd) , ...);}, net);
+    //auto net = std::apply([&](auto... x){ return std::make_tuple(x.map(rd)...);}, net);
 
-    for (int _ = 0; _ < 10000; ++_) {
-        // auto L1 = dot(X,  w0).map(g); // 4 * 4
-        // auto L2 = dot(L1, w1).map(g); // 4 * 1
-        // auto L2D = (y - L2) * L2.map(g_);
-        // auto L1D = dot(L2D, w1.T()) * L1.map(g_);
-        // w1 += eta * dot(L1.T(), L2D);
-        // w0 += eta * dot( X.T(), L1D);
+    //std::apply([&](const auto&... x){ ((std::cout << x), ...);}, net);
 
-        // backwards(y, forwards(X, w0, w1), L1, w1, L0, w0);
-        // auto x = nn::forward(X, w0, w1);
-        //auto x = nn::train(X, y, w0, w1);
-        nn::train(X, y, w0, w1);
-        //auto A = nn::forwards(X, w0, w1);
-        //std::cout << "a:\n" << A;
-        //std::cout << "y:\n" << y;
-        //std::cout << "MSE(  1): " << nn::get_mse(A(1), y(1)) << "\n";
-        //std::cout << "MSE(all): " << nn::get_mse(A, y) << "\n";
-
-        //char *name = abi::__cxa_demangle(typeid(x).name(),
-        //                                 nullptr,
-        //                                 nullptr,
-        //                                 nullptr);
-        //std::cout << "🦄: " << name << "\n";
-
-        //std::cout << "a:\n" << std::get<0>(x);
-        //std::cout << "y:\n" << y;
-        //std::cout << "MSE(  1): " << nn::get_mse(std::get<0>(x)(1), y(1)) << "\n";
-        //std::cout << "MSE(all): " << nn::get_mse(std::get<0>(x), y) << "\n";
+    for (int _ = 0; _ < 1000000; ++_) {
+        std::apply([&](auto&...x) { nn::train(X, y, x...); }, net);
     }
 
-    auto A = nn::forwards(X, w0, w1);
+    auto A = std::apply([&](auto&...x) { return nn::forwards(X, x...); }, net);
     std::cout << A;
     std::cout << "MSE(all): " << nn::get_mse(A, y) << "\n";
-
-
-    //auto L1 = dot(X,  w0).map(nn::g);
-    //auto L2 = dot(L1, w1).map(nn::g);
-    //std::cout << L2;
-
-    #ifdef NDEBUG
-    std::cout << "NDEBUG\n";
-    #endif
 
     return 0;
 }
