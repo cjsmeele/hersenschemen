58d11e68e3eb370937fc9413e29294b470a4fb03
Chris Smeele <cj.smeele@xs4all.nl>
2017-12-17 21:31:43 +0100

    Forgot one...

--- neuralnet-f/data-to-net-input.pl
+++ neuralnet-f/data-to-net-input.pl
@@ -1,5 +1,21 @@
 #!/usr/bin/env perl
 
+# data-to-net-input.pl - Convert iris (or other data) to nn:: vector code.
+# Copyright (C) 2017, Chris Smeele and Jan Halsema.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
 use 5.12.0;
 use warnings;
 
